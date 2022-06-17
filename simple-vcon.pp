616385585aa6ce37917ceeafb14b7a71a5afb828
dgpetrie <dpetrie@sipez.com>
2022-06-17 15:32:48 -0400

    line wrapped version of vcon for inclusion in ID

---
+++ simple-vcon.pp
@@ -0,0 +1,9 @@
+{
+  "vcon" : 0.0.1,
+  "text" : "Really long lineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeh",
+  "subject" : "test kramdown include to appendix"
+}
+
