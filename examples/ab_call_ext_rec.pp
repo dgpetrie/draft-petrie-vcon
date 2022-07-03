fc8fd60047e33e03cc405083f19ebaeaecc3187a
dgpetrie <dpetrie@sipez.com>
2022-07-03 19:05:31 -0400

    Example 2 party call vCon with externally referenced recording

---
+++ examples/ab_call_ext_rec.pp
@@ -0,0 +1,60 @@
+{
+  "vcon": "0.0.1",
+  "parties": [
+    {
+      "tel": "+12345678901",
+      "name": "Alice"
+    },
+    {
+      "tel": "+19876543210",
+      "name": "Bob"
+    }
+  ],
+  "dialog": [
+    {
+      "type": "recording",
+      "start": "2022-06-21T17:53:26.000+00:00",
+      "duration": 4.72,
+      "parties": [
+        0,
+        1
+      ],
+      "url": "https://raw.githubusercontent.com/dgpetrie/draft-pe
+        etrie-vcon/main/examples/ab_call.wav",
+      "mimetype": "audio/x-wav",
+      "filename": "ab_call.wav",
+      "key": "AAAABLteJFQZZ4HS81-81E_QF9AAAAAAn4tOuvSvTaMooew1G8D
+        Dnzk4QWZQM8reHNh_-StPUOZE",
+      "signature": "AAAABBPwpeMcksqrQQrpxpHBRZAqRP4pJyHPLxRE-pwbs
+        sHsSUknC839isw-XzrTzHeGh5Lx9lFMDrib52hu1v0bJU7seVMpZc3eV6
+        UsdGqw_IqQB4KTN_V3K_J7SkWFpGuE0O9PdS5R4X8LZ_PdXBs6H6Hsrd8
+        2jsjZMTCEeIPTBU5vBk-8tJFlfTj6tTTY2DEc3aDs6VILnrp5YXkdykv6
+        puIPiIXwz5EIVhTP8E6kysKaGaMQ-yRjllvPmwvPfWc75jvOJmnRHzE_7
+        v3y0H1X2UKaLT4WdU9ZEsjgvBluoTB1lPR0YtFr6F0_qCajCK8mbu9Wu3
+        VqPPdCVRtKtZLDRkpdxZLyTKXpxpUCgxq53qDYhjqhuQJqcNBYH_-Gi3h
+        SaXKCr7Hei9VDv0N5AvExN3gq2DCqLnFNGC8WJg6EmLUjnejgriJcuDGq
+        iHat7PDcyTncox990pd1R1EDL3jwKSvvruDeOuwroOTWEXCoGO2PlamRu
+        W3tpVejqB-4hUtcnA23uubNL1ru6WOsYB8EB5YnrnC0okM-MI0PN-cgAR
+        7MGE79ODG6cVTqzcxEN5O_QMM1z0tVW1LHPXx9YXEdsrtgDYkt2APgp52
+        k_3PsMsiGcmKSGud0SYoJtNcW7UFzVXJff6jhLpzw_CwDJabE53I3AXyb
+        ZNmC5zVe8w9NJgiCi5lV_5j65mcWO_zW_Ehu_9z8xJn06DIcTa-XnFH-y
+        T-pzc2OwGMP4H6qt4gRY6EsVGQJ4fnI0ypsy64cU4-JE9Q1GCD20YdPF9
+        EMWHlZMGUyX08OAOJ3VVY11N5kx6ZnOoDh_j3L9d19ptye-M1MyFVfoCx
+        6fU6wo7BKoyo0o2shhc3tbE6pB6zPFb1AYz1s_aa67Fb9-7Y1Wjpytavp
+        HZrHtBitZeZAMTkYuANDB0jq9eVnI-fZFtufjbBipT16Q0k1oYrrBdGGi
+        6UWiFCIe8KGQTjw6aPyWdquJ6bZ80ky4XA1RK5kccybNe4x2CRQVmf0Il
+        q51iJVzezlO7PRDvLEniXBR7pPPHr4HdP-KDgIsmqLLPZsS8RTqqugLOj
+        -oZREJ5DjECmS-bVnQJhAm_WWqTKUKuHVsYtOeXoSLbHlwC636svGCVr7
+        ef12Xft8dNZ_OshaDckgyeXp9mHBgDvmvlGRRSeWBlmKUNFSjRz4X5tA9
+        lP8uEupTVfh_pFTGK69H5fUzP6LLYJue3CMEBcIrQ4J99Srjgd-JOa96l
+        MZKaMJgyzbdxzozSmhwnhDvKgJhI7ig91r5PGJOu_3049-AM7v2rwjOcz
+        LpSlHngQQFRD01D3gWdvfUFM5dNfjla7KM1Mlfq3Anpi9kXkpBzyAGR2H
+        IFELJlZD5wuK7neKbWv2KvpXPToF2z2W37_x4vgW4K5IgZgl5LBEyoE5e
+        ntS3CX3XBqBNInWnYvjyHmzPSatJEctRcltmPX-iuzgH-39ZKGKdeSiKG
+        1o810",
+      "alg": "LMOTS_SHA256_N32_W8"
+    }
+  ],
+  "analysis": [],
+  "attachments": []
+}
