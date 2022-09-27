c289d2f363ae00255925af14fb54defc077c234b
dgpetrie <dpetrie@sipez.com>
2022-09-27 18:05:52 -0400

    Added example email vcon thread with text bodies

---
+++ examples/ab_email_prob_followup_text_thread.pp
@@ -0,0 +1,65 @@
+{
+  "vcon": "0.0.1",
+  "parties": [
+    {
+      "mailto": "a@example.com",
+      "name": "Alice"
+    },
+    {
+      "mailto": "b@example.com",
+      "name": "Bob"
+    }
+  ],
+  "dialog": [
+    {
+      "type": "text",
+      "start": "2022-09-23T23:24:59.000+00:00",
+      "duration": 0,
+      "parties": [
+        0,
+        1
+      ],
+      "mimetype": "text/plain",
+      "encoding": "None",
+      "body": "Hi Bob:\nI just wanted to follow up to see that yo
+        our account is ok now.\n\nRegards,\nAlice\n\n"
+    },
+    {
+      "type": "text",
+      "start": "2022-09-23T23:34:32.000+00:00",
+      "duration": 0,
+      "parties": [
+        1,
+        0
+      ],
+      "mimetype": "text/plain; charset=UTF-8",
+      "encoding": "None",
+      "body": "Hi Alice:\nAll is fine. Thanks.\n\n    On Friday, 
+         September 23, 2022, 23:24:59 PM UTC, Alice <a@example.co
+        > wrote:  \n \n  Hi Bob:\n  I just wanted to follow up to
+        see that your account is ok now.\n  \n  Regards,\n  Alice
+        n"
+    },
+    {
+      "type": "text",
+      "start": "2022-09-23T23:38:12.000+00:00",
+      "duration": 0,
+      "parties": [
+        0,
+        1
+      ],
+      "mimetype": "text/plain",
+      "encoding": "None",
+      "body": "Awesome!\n\n    On Friday, September 23, 2022, 23:
+        :34: PM UTC, Bob <b@example.com> wrote:  \n\n  Hi Alice:\
+          All is fine. Thanks.\n  \n      On Friday, September 23
+         2022, 23:24:59 PM UTC, Alice <a@example.com> wrote:  \n 
+         \n    Hi Bob:\n    I just wanted to follow up to see tha
+         your account is ok now.\n    \n    Regards,\n    Alice\n
+    }
+  ],
+  "analysis": [],
+  "attachments": [],
+  "subject": "Account issue followup",
+  "uuid": "018380f8-60db-8464-973a-91e26eb8001b"
+}
