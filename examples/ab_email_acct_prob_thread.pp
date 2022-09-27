e3a8354ad191ea3865b772b59d99869c011a1455
dgpetrie <dpetrie@sipez.com>
2022-09-27 15:46:48 -0400

    Added email thread example vcon with multipart bodies

---
+++ examples/ab_email_acct_prob_thread.pp
@@ -0,0 +1,121 @@
+{
+  "vcon": "0.0.1",
+  "parties": [
+    {
+      "mailto": "b@example.com",
+      "name": "Bob"
+    },
+    {
+      "mailto": "a@example.com",
+      "name": "Alice"
+    }
+  ],
+  "dialog": [
+    {
+      "type": "text",
+      "start": "2022-09-23T21:44:25.000+00:00",
+      "duration": 0,
+      "parties": [
+        0,
+        1
+      ],
+      "mimetype": "multipart/mixed; \n\tboundary=\"----=_Part_388
+        8314_1108189513.1663969465698\"",
+      "encoding": "None",
+      "body": "------=_Part_388314_1108189513.1663969465698\n\rCo
+        ontent-Type: multipart/alternative;\n\r\tboundary=\"----=
+        Part_388313_805631480.1663969465697\"\n\r\n\r------=_Part
+        388313_805631480.1663969465697\n\rContent-Type: text/plai
+        ; charset=UTF-8\n\rContent-Transfer-Encoding: 7bit\n\r\n\
+        Alice:Please find the image attached.\n\r\n\rRegards,Bob\
+        \r\n\r------=_Part_388313_805631480.1663969465697\n\rCont
+        nt-Type: text/html; charset=UTF-8\n\rContent-Transfer-Enc
+        ding: 7bit\n\r\n\r<html><head></head><body><div class=\"y
+        hoo-style-wrap\" style=\"font-family:Helvetica Neue, Helv
+        tica, Arial, sans-serif;font-size:16px;\"><div dir=\"ltr\
+         data-setdir=\"false\"><div><div dir=\"ltr\" style=\"font
+        family: &quot;Helvetica Neue&quot;, Helvetica, Arial, san
+        -serif; font-size: 16px;\">Alice:</div><div dir=\"ltr\" s
+        yle=\"font-family: &quot;Helvetica Neue&quot;, Helvetica,
+        Arial, sans-serif; font-size: 16px;\">Please find the ima
+        e attached.<br></div><div dir=\"ltr\" style=\"font-family
+         &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif
+         font-size: 16px;\"><br></div><div dir=\"ltr\" style=\"fo
+        t-family: &quot;Helvetica Neue&quot;, Helvetica, Arial, s
+        ns-serif; font-size: 16px;\">Regards,</div><div dir=\"ltr
+        " style=\"font-family: &quot;Helvetica Neue&quot;, Helvet
+        ca, Arial, sans-serif; font-size: 16px;\">Bob</div></div>
+        br></div></div></body></html>\n\r------=_Part_388313_8056
+        1480.1663969465697--\n\r\n\r------=_Part_388314_110818951
+        .1663969465698\n\rContent-Type: image/png\n\rContent-Tran
+        fer-Encoding: base64\n\rContent-Disposition: attachment; 
+        ilename=\"=?UTF-8?b?c2NyZWVuc2hvdC5wbmc=?=\"\n\rContent-I
+        : <c4c28087-6fa1-395e-e50f-dcefd3d6bf96@yahoo.com>\n\r\n\
+        iVBORw0KGgoAAAANSUhEUgAAACYAAAAeCAIAAADPdvs5AAAAA3NCSVQIC
+        jb4U/gAAAAGXRFWHRT\n\rb2Z0d2FyZQBnbm9tZS1zY3JlZW5zaG907wO
+        PgAAALpJREFUSInl1t0NgzAMBOC7qPvP1gW6Qjdo\n\rj4f0J4qAJI6NR
+        vniE8+mwCvtzv6imTnyf1K/UclHUFWjIvaIL3CHCABSPKKtJckWfY6zw+
+        j5dq\n\rISdVIzmj2klzTZG2Rme7NKgOwY6qPrMcUs+2PqchL14P+oyz+
+        Hx77K5Sv8xyxAyZ0uBaxn/SpcU\n\r8NxcosAut1Y3NtjVX7UjZlmpgWT
+        FyvVQPKB16tSqW53bFnKtyyZvYT3rSsAWADWAlOjseYFfwAA\n\rAABJR
+        5ErkJggg==\n\r\n\r------=_Part_388314_1108189513.16639694
+        5698--\n\r"
+    },
+    {
+      "type": "text",
+      "start": "2022-09-23T21:44:59.000+00:00",
+      "duration": 0,
+      "parties": [
+        1,
+        0
+      ],
+      "mimetype": "multipart/alternative; \n\tboundary=\"----=_Pa
+        art_390234_811845464.1663969499935\"",
+      "encoding": "None",
+      "body": "------=_Part_390234_811845464.1663969499935\n\rCon
+        ntent-Type: text/plain; charset=UTF-8\n\rContent-Transfer
+        Encoding: 7bit\n\r\n\r Hi Bob:Got it. Thanks.\n\r\n\r    
+        n Friday, September 23, 2022, 9:44:40 PM UTC, Bob <b@exam
+        le.com> wrote:  \n\r \n\r Alice:Please find the image att
+        ched.\n\r\n\rRegards,Bob\n\r  \n\r------=_Part_390234_811
+        45464.1663969499935\n\rContent-Type: text/html; charset=U
+        F-8\n\rContent-Transfer-Encoding: 7bit\n\r\n\r<html><head
+        </head><body><div class=\"ydp89e479a0yahoo-style-wrap\" s
+        yle=\"font-family:Helvetica Neue, Helvetica, Arial, sans-
+        erif;font-size:16px;\"><div></div>\n\r        <div><div><
+        iv dir=\"ltr\" style=\"font-family: Helvetica Neue, Helve
+        ica, Arial, sans-serif; font-size: 16px;\">Hi Bob:</div><
+        iv dir=\"ltr\" style=\"font-family: Helvetica Neue, Helve
+        ica, Arial, sans-serif; font-size: 16px;\">Got it. Thanks
+        </div></div><br></div><div><br></div>\n\r        \n\r    
+           </div><div id=\"ydpdc5150a3yahoo_quoted_4829810715\" c
+        ass=\"ydpdc5150a3yahoo_quoted\">\n\r            <div styl
+        =\"font-family:'Helvetica Neue', Helvetica, Arial, sans-s
+        rif;font-size:13px;color:#26282a;\">\n\r                \
+        \r                <div>\n\r                    On Friday,
+        September 23, 2022, 9:44:40 PM UTC, Bob &lt;b@example.com
+        gt; wrote:\n\r                </div>\n\r                <
+        iv><br></div>\n\r                <div><br></div>\n\r     
+                  <div><div id=\"ydpdc5150a3yiv4578005613\"><div>
+        div style=\"font-family:Helvetica Neue, Helvetica, Arial,
+        sans-serif;font-size:16px;\" class=\"ydpdc5150a3yiv457800
+        613yahoo-style-wrap\"><div dir=\"ltr\"><div><div dir=\"lt
+        \" style=\"font-family:Helvetica, Arial, sans-serif;font-
+        ize:16px;\">Alice:</div><div dir=\"ltr\" style=\"font-fam
+        ly:Helvetica, Arial, sans-serif;font-size:16px;\">Please 
+        ind the image attached.<br></div><div dir=\"ltr\" style=\
+        font-family:Helvetica, Arial, sans-serif;font-size:16px;\
+        ><br></div><div dir=\"ltr\" style=\"font-family:Helvetica
+         Arial, sans-serif;font-size:16px;\">Regards,</div><div d
+        r=\"ltr\" style=\"font-family:Helvetica, Arial, sans-seri
+        ;font-size:16px;\">Bob</div></div><br></div></div></div><
+        div></div>\n\r            </div>\n\r        </div></body>
+        /html>\n\r------=_Part_390234_811845464.1663969499935--\n
+        r"
+    }
+  ],
+  "analysis": [],
+  "attachments": [],
+  "subject": "Account problem",
+  "uuid": "0183806b-f18f-8c84-973a-91e26eb8001b"
+}
